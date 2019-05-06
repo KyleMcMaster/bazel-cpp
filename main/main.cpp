471bddd60eb92d31731977191ad9a6a52ad63ae2
kmcmasterppt <kmcmaster@parkplacetech.com>
2019-05-05 19:56:45 -0600

    inital commit

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+/*
+* author: kylermcmaster@gmail.com
+* build using: bazel build ///main:hello-bazel
+*/
+#include <iostream> 
+#include <cstdio>
+
+int main()      
+{    
+    std::cout << "Hello Bazel! ";
+    std::getchar();
+    return 0;      
+}
