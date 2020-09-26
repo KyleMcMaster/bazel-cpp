8fefe8741faa60fd80a45a44ca8575e1f7cca66f
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-09-26 14:22:10 -0700

    feat(graph): add graph node

---
+++ graph/graph.cpp
@@ -0,0 +1,25 @@
+﻿// graph.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include "graph.h"
+
+using namespace std;
+
+int main()
+{
+	// a -> b -> c
+	Node c = Node("c", NULL);
+	Node b = Node("b", &c);
+	Node a = Node("a", &b);
+
+	Node* n = &a;
+
+	while (n != NULL)
+	{
+		std::cout << n->value << std::endl;
+		n = n->next;
+	}
+
+	return 0;
+}
---
+++ graph/graph.h
@@ -0,0 +1,29 @@
+﻿// graph.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+class Node
+{
+public:
+	Node(std::string v, Node* n);
+	~Node();
+	std::string value;
+	Node* next;
+
+private:
+
+};
+
+Node::Node(std::string v, Node* n)
+{
+	value = v;
+	next = n;
+}
+
+Node::~Node()
+{
+}
+
